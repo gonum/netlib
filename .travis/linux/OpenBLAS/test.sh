eb67595f573b1b4e2ad9457cd120d6c809f483f6
kortschak <dan.kortschak@adelaide.edu.au>
2017-12-07 12:30:24 +1030

    travis: ensure go test knows where cgo libs are

--- .travis/linux/OpenBLAS/test.sh
+++ .travis/linux/OpenBLAS/test.sh
@@ -2,6 +2,7 @@ set -ex
 
 go env
 go get -d -t -v ./...
+export CGO_LDFLAGS="-L/usr/lib -lopenblas"
 go test -a -v ./...
 if [[ $TRAVIS_SECURE_ENV_VARS = "true" ]]; then bash -c "$GOPATH/src/gonum.org/v1/netlib/.travis/test-coverage.sh"; fi
 
