2c9afffb758c5da804d198be5cd93543aad7b1f7
Dan Kortschak <dan@kortschak.io>
2019-03-30 09:51:30 +1030

    travis: run install and tests in new shells
    This had the effect of killing the travis script when a t.Fatal was hit.
    As noted in the comment that was removed - a non-zero exit status makes
    set -e exit, but this exits from the build script.

--- .travis/linux/OpenBLAS/test.sh
+++ .travis/linux/OpenBLAS/test.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 set -ex
 
 go env
@@ -5,13 +7,3 @@ go get -d -t -v ./...
 export CGO_LDFLAGS="-L/usr/lib -lopenblas"
 go test -a -v ./...
 if [[ $TRAVIS_SECURE_ENV_VARS = "true" ]]; then bash -c "$GOPATH/src/gonum.org/v1/netlib/.travis/test-coverage.sh"; fi
-
-# travis compiles commands in script and then executes in bash.  By adding
-# set -e we are changing the travis build script's behavior, and the set
-# -e lives on past the commands we are providing it.  Some of the travis
-# commands are supposed to exit with non zero status, but then continue
-# executing.  set -x makes the travis log files extremely verbose and
-# difficult to understand.
-# 
-# see travis-ci/travis-ci#5120
-set +ex
